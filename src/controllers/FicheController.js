eba0330dde423e9296eff513d823f6fa1b1ef04a
Nabila <nabiladehmani062@gmail.com>
2021-04-28 17:26:37 +0200

    placeId

---
+++ src/controllers/FicheController.js
@@ -0,0 +1,45 @@
+const fiche = require('../models/fiche')
+
+
+
+exports.findPlaces = (req, res) => {
+    const { placeId } = req.params;
+  
+    const user = req.body.id_user;
+    // l'envoi de id permet de référencier les users
+   
+  
+    fiche.findPlaces(placeId, (error, result) => {
+      if(error) {
+        res.status(400).json({ message:"La ressource demandée n'existe pas" });
+      } 
+      else {
+       
+        const array = [];
+        for (let i = 0; i < result.length; i++) {
+          const obj = {
+            id_place: result[i].id_place,
+            city_id: result[i].city_id,
+            user_id: result[i].user_id,
+            name: result[i].name,
+            description: result[i].description,
+            rooms: result[i].rooms,
+            bathrooms: result[i].bathrooms,
+            max_guests: result[i].max_guests,
+            price_by_night: result[i].price_by_night,
+            available: result[i].available,
+          };
+        
+          array.push(obj);
+        }
+        if(array.length ==0){
+            res.status(400).json({ message:"La ressource demandée n'existe pas" });
+          }
+          else{
+                 res.status(201).json(array);
+          }
+     
+      }
+    });
+  };
+  
--- src/routers/index.js
+++ src/routers/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/BookingController');
 const  signupController = require('../controllers/signupController');
+const ficheController = require('../controllers/FicheController')
 
 router.get('/', controller.controller);
 router.get('/api', (req, res) => {
@@ -12,6 +13,7 @@ router.get('/api', (req, res) => {
 
 
 router.post('/api/signup', signupController.newAccount);
+router.get('/api/places/:placeId',ficheController.findPlaces)
 
 
 
