46b4175e0d956dd9240a7ab987d700a553f378e1
zeliha konak <zelihaozsiray@gmail.com>
2021-04-28 10:59:27 +0200

    update place

--- src/controllers/PlaceUpdateController.js
+++ src/controllers/PlaceUpdateController.js
@@ -1,4 +1,5 @@
 const place = require('../models/UserSignin');
+const util = require('../utils/utils');
 
 exports.updatePlace = (request, response) => {
   const { placeId } = request.params;
@@ -14,12 +15,18 @@ exports.updatePlace = (request, response) => {
       response.status(400).json({
         message: "La ressource demandée n'existe pas",
       });
+    } else if (util.variblePlace(request.body).length > 0) {
+      response.status(400).json({ message: `Le champ ${util.variblePlace(request.body)} n'existe pas` });
+
+      // eslint-disable-next-line max-len
+    } else if (util.theTypePlace(request.body).length > 0) {
+      response.status(400).json({ message: `Le champ ${util.theTypePlace(request.body)} doit être correct type` });
     } else {
       place.updatePlace(request.body, placeId, (errorUpdate, result) => {
         if (errorUpdate) {
           response.status(400).json({ message: errorUpdate.message });
         } else {
-            console.log(placeInfo);
+          console.log(placeInfo);
           response.status(200).json({
 
             id: placeInfo[0].place_id,
