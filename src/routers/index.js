b2a0c42ea74264309e55fb753a827dbdf26393b5
samira <houacine.samira@gmail.com>
2021-04-30 14:59:38 +0200

    reserver une chambre

--- src/routers/index.js
+++ src/routers/index.js
@@ -2,13 +2,14 @@ const express = require('express');
 
 const router = express.Router();
 const controller = require('../controllers/BookingController');
-const isAuth =require ('../middlewares/isAuth');
+const isAuth = require('../middlewares/isAuth');
 
 const UserSignInController = require('../controllers/UserSignInController');
 const PlaceUpdateController = require('../controllers/PlaceUpdateController');
 
 const roomController = require('../controllers/roomController');
-const roomBooking = require ('../controllers/roomBooking');
+const roomBooking = require('../controllers/roomBooking');
+
 router.get('/', controller.controller);
 router.get('/api', (req, res) => {
   res.json({ message: 'hello, world!' });
@@ -20,6 +21,7 @@ router.get('/api/places/:id', roomController.findRooms);
 router.post('/api/signin', UserSignInController.signin);
 router.patch('/api/places/:placeId', PlaceUpdateController.updatePlace);
 router.post('/api/places', isAuth, roomController.addRooms);
-router.post('/api/bookings',roomBooking.touristBooking);
+router.post('/api/bookings', roomBooking.touristBooking);
+router.delete('/api/bookings/:bookingID');
 
 module.exports = router;
