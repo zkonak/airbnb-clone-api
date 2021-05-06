863cf12b8991f5980e67910743844f45d7e2845c
zeliha konak <zelihaozsiray@gmail.com>
2021-05-06 12:11:08 +0200

    modification sql

--- src/models/filter.js
+++ src/models/filter.js
@@ -5,12 +5,12 @@ const db = require('../config/database');
 // // ETQ visiteur, je veux rechercher un appartement ou une maison disponible entre deux dates
 
 exports.filterRoom = (filtre, callback) => {
-  let query = 'SELECT *,place.name place_name,city.name city_name FROM airbnb.place  inner join city  ';
+  let query = 'SELECT *,place.name place_name,city.name city_name FROM airbnb.place  inner join city WHERE id_city=city_id   ';
   if (filtre.check_in && filtre.check_out) {
-    query = `${query} WHERE id_city=city_id and available BETWEEN '${filtre.check_in}' And '${filtre.check_out}';`;
+    query = `${query} and available BETWEEN '${filtre.check_in}' And '${filtre.check_out}';`;
     console.log(query);
   } else if (filtre.cityName) {
-    query = `${query}  where id_city=city_id and name='${filtre.cityName}'`;
+    query = `${query}  and city.name='${filtre.cityName}'`;
   }
 
   db.query(query, (error, result) => {
